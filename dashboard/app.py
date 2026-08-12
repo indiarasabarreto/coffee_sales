3513a828abe67e18bb9f946279628333b2069b56
Indiara dos Santos Sá Barreto <66640743+indiarasabarreto@users.noreply.github.com>
2026-08-12 11:33:38 +0000

    gráfico modificado

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 from pathlib import Path
 
+df = pd.read_csv(Path(__file__).parent / 'coffee_data.csv')
+
+st.title("Dashboard de Vendas")
+
+cafe_selecionado = st.selectbox("Escolha o café", df['coffee_name'].unique())
+
 # Filtra os dados pelo café selecionado (que você já tem)
 df_filtrado = df[df['coffee_name'] == cafe_selecionado]
 
@@ -13,15 +19,8 @@ col1.metric("💰 Receita Total", f"R$ {df_filtrado['money'].sum():,.2f}")
 col2.metric("📦 Total de Vendas", len(df_filtrado))
 col3.metric("🎯 Ticket Médio", f"R$ {df_filtrado['money'].mean():.2f}")
 
-df = pd.read_csv(Path(__file__).parent / 'coffee_data.csv')
-
-
-st.title("Dashboard de Vendas")
-
-coffee_name = st.selectbox("Escolha o café", df['coffee_name'].unique())
-filtered_data = df[df['coffee_name'] == coffee_name]
-st.write(filtered_data)
-st.bar_chart(filtered_data.groupby('date')['money'].sum())
+st.write(df_filtrado)
+st.bar_chart(df_filtrado.groupby('date')['money'].sum())
 
 
 # run with streamlit run .\app.py
